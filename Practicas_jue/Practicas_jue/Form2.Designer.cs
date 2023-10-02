77b1c25bc375a940ddb531cc2fd5534bdc4e73f1
Stiven461 <143130153+Stiven461@users.noreply.github.com>
2023-10-02 15:49:48 -0500

    Add files via upload

--- Practicas_jue/Practicas_jue/Form2.Designer.cs
+++ Practicas_jue/Practicas_jue/Form2.Designer.cs
@@ -90,6 +90,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button15
             // 
@@ -163,6 +164,7 @@
             this.button8.TabIndex = 7;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button7
             // 
@@ -172,6 +174,7 @@
             this.button7.TabIndex = 6;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button6
             // 
@@ -181,6 +184,7 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button5
             // 
@@ -190,6 +194,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button4
             // 
@@ -199,6 +204,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button2
             // 
@@ -218,6 +224,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form2
             // 
--- Practicas_jue/Practicas_jue/Form2.cs
+++ Practicas_jue/Practicas_jue/Form2.cs
@@ -35,18 +35,80 @@ namespace Practicas_jue
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            vaidarVacios(button2, button1);
+            vaidarVacios(button2, button3);
+            vaidarVacios(button2, button6);
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-            if (button8.Text == " ")
-             {
-                button8.Text = button12.Text;
-                button12.Text = String.Empty;
+            //if (button16.Text == "")
+            // {
+            //    button16.Text = button12.Text;
+            //    button12.Text = "";
+            //    return;
+
+            //}
+            vaidarVacios(button12,button16);
+          
+        }
+
+        public void vaidarVacios(Button boton1, Button boton2)
+        {
+            if (boton2.Text == "")
+            {
+                boton2.Text = boton1.Text;
+                boton1.Text = "";
                 return;
 
             }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            vaidarVacios(button8, button12);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            vaidarVacios(button4, button8);
+            vaidarVacios(button4, button3);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            vaidarVacios(button3, button4);
+            vaidarVacios(button3, button2);
+            vaidarVacios(button3, button7);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vaidarVacios(button1, button2);
+            vaidarVacios(button1, button5);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            vaidarVacios(button5, button1);
+            vaidarVacios(button5, button6);
+            vaidarVacios(button5, button9);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            vaidarVacios(button6, button2);
+            vaidarVacios(button6, button5);
+            vaidarVacios(button6, button7);
+            vaidarVacios(button6, button10);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            vaidarVacios(button7, button3);
+            vaidarVacios(button7, button6);
+            vaidarVacios(button7, button8);
+            vaidarVacios(button7, button11);
         }
     }
 }
